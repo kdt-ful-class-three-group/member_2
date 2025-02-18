d0499c3e0be6b00ae5650917a15895d0a62694e2
kimyohun <yohunkim0@gmail.com>
2025-02-18 18:03:02 +0900

    POST를 확인하기 위한 js

--- appear.js
+++ appear.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 
+
 // JSON 갖고 오기 위한 서버 구동
 const server = http.createServer(function(request, response){
   if(request.method === 'GET'){
@@ -12,6 +13,19 @@ const server = http.createServer(function(request, response){
       const filePath = fs.readFileSync('./appear.js')
       response.writeHead(200,{'Content-Type': 'text/javascript'}).end(filePath)
     }
+  }
+
+  // POST 방식 
+  if(request.metod === 'POST'){
+    if(request.url === '/formGET'){
+      // 들어오는 데이터 확인
+      response.on('data',function(data){
+        console.log(data);
+      })
+      const filePath = fs.readFileSync('./formGET.html')
+      response.writeHead(200,{'Content-Type': 'text/html'}).end(filePath)
+    }
+
   }
 })
 
