cf6f1214322a1dcd59dc5386c6d2820fac169065
kimyohun <yohunkim0@gmail.com>
2025-02-18 16:27:35 +0900

    서버 구현 후 테스트 실행

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("안녕하세요 테스트");
+const http = require('http')
+const fs = require('fs')
+
+// JSON 갖고 오기 위한 서버 구동
+const server = http.createServer(function(request, response){
+  if(request.method === 'GET'){
+    if(request.url === '/'){
+      console.log("안녕하세요")
+    }
+  }
+})
+
+// 3000 port start
+server.listen(3000,function(){
+  console.log("http://localhost:3000 실행중")
+})
