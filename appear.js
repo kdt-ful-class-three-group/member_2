4a8754746155b541bce0a7be7a30127211e8ff0a
kimyohun <yohunkim0@gmail.com>
2025-02-19 10:26:31 +0900

    querystring을 parse하여 객체 변환

--- appear.js
+++ appear.js
@@ -1,6 +1,22 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
 
+function stringSplit(data){
+  let dataArray = [];
+  // &로 fileName , fileContent 나눔
+  let dataGet = data.toString()
+  let result = dataGet.split('&')
+  // &가 먼저 잘 되는지 확인
+  // console.log(result)
+  // 배열 순회해서 =이 되면 데이터를 추출
+  result.forEach(element => {
+    let value = element.split('=')[1]
+    dataArray.push(value)
+  });
+  // 데이터만 추출
+  return dataArray;
+}
 
 // JSON 갖고 오기 위한 서버 구동
 const server = http.createServer(function(request, response){
@@ -21,19 +37,10 @@ const server = http.createServer(function(request, response){
     if(request.url === '/formGET'){
       // 들어오는 데이터 확인
       request.on('data',function(data){
-        let dataArray = [];
-        // &로 fileName , fileContent 나눔
-        let dataGet = data.toString()
-        let result = dataGet.split('&')
-        // &가 먼저 잘 되는지 확인
-        console.log(result)
-        // 배열 순회해서 =이 되면 데이터를 추출
-        result.forEach(element => {
-          let value = element.split('=')[1]
-          dataArray.push(value)
-        });
-        // =에 제거하고 값만 추출
-        console.log(dataArray)
+        const dataInit = data.toString();
+        const qsCheck = qs.parse(dataInit)
+        // const result = stringSplit(data)
+        console.log(qsCheck);
 
       })
       const filePath = fs.readFileSync('./formGET.html')
