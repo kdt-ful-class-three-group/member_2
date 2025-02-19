652d2eec7ab73d4fbe19f488e446eb4edd9d5529
kimyohun <yohunkim0@gmail.com>
2025-02-19 10:04:16 +0900

    querystring에서 데이터만 추출

--- appear.js
+++ appear.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 
 // JSON 갖고 오기 위한 서버 구동
 const server = http.createServer(function(request, response){
+  // GET 방식 
   if(request.method === 'GET'){
     if(request.url === '/'){
       const filePath = fs.readFileSync('./index.html')
@@ -20,7 +21,20 @@ const server = http.createServer(function(request, response){
     if(request.url === '/formGET'){
       // 들어오는 데이터 확인
       request.on('data',function(data){
-        console.log(data);
+        let dataArray = [];
+        // &로 fileName , fileContent 나눔
+        let dataGet = data.toString()
+        let result = dataGet.split('&')
+        // &가 먼저 잘 되는지 확인
+        console.log(result)
+        // 배열 순회해서 =이 되면 데이터를 추출
+        result.forEach(element => {
+          let value = element.split('=')[1]
+          dataArray.push(value)
+        });
+        // =에 제거하고 값만 추출
+        console.log(dataArray)
+
       })
       const filePath = fs.readFileSync('./formGET.html')
       response.writeHead(200,{'Content-Type': 'text/html'}).end(filePath)
