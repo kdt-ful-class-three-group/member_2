525f2ab6e0bb61ab6aa85f30718e56b1fcf27e8c
kimyohun <yohunkim0@gmail.com>
2025-02-19 17:15:27 +0900

    JSON 데이터를 받아서 파일을 생성

--- createFile.js
+++ createFile.js
@@ -3,4 +3,20 @@
  TODO : readFile에서 먼저 받고서 시작
  */
 
- console.log("create js 생성완료");
+const fs = require('fs')
+
+// try catch 사용하여 확인
+// try {
+//   fs.appendFileSync('message.txt', 'data to append ');
+//   console.log('The "data to append" was appended to file!');
+// } catch (err) {
+//   /* Handle the error */
+// }
+
+function createNewFile(data){
+  fs.appendFileSync('data.JSON', data);
+  console.log("데이터 생성됨");
+
+}
+
+module.exports = createNewFile
